9b19709d0a5b84b78eff6836b57a0e9aba992776
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2026-03-23 20:32:55 +0300

    Доработана верстка фильтра

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,17 +1,3 @@
-/* import styles from './Filter.module.css';
-
-export default function Filter() {
-    return (
-        <div className={styles.centerblock__filter}>
-            <div className={styles.filter__title}>Искать по:</div>
-            <div className={styles.filter__button}>исполнителю</div>
-            <div className={styles.filter__button}>году выпуска</div>
-            <div className={styles.filter__button}>жанру</div>
-        </div>
-    );
-}
- */
-
 'use client';
 
 import { data } from '@/app/data';
